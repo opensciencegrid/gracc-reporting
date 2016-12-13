62573e44209ea41c3f0f70a1303a7f9de916dc17
sbhat <sbhat@fnal.gov>
2016-12-13 16:19:12 -0600

    Made some code changes and added comments

--- OSG_Reports/OSGReporter_GRACC.py
+++ OSG_Reports/OSGReporter_GRACC.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python
 
-import xml.etree.ElementTree as ET
-from datetime import timedelta, date
-import urllib2
-import ast
 import os
 import inspect
 import re
-import smtplib
-import email.utils
-from email.mime.text import MIMEText
-import datetime
-import logging
-import json
-import operator
 import traceback
 import sys
 
-from elasticsearch_dsl import Search, Q
-
+from elasticsearch_dsl import Search
 
 parentdir = os.path.dirname(
     os.path.dirname(
@@ -48,9 +36,10 @@ class OSGReporter(Reporter):
     def __init__(self, config, report_type, limit, start, end=None, isSum=True,
                  verbose=False):
         Reporter.__init__(self, config, start, end, verbose, raw=False)
-        self.header=["Project Name","PI","Institution","Field of Science","Wall Hours"]
+        self.header = ["Project Name", "PI", "Institution", "Field of Science",
+                     "Wall Hours"]
         self.logfile = logfile
-        self.logger = self.setupgenLogger("ProbeReport")
+        self.logger = self.setupgenLogger("OSGReport")
 
         try:
             self.client = self.establish_client()
@@ -112,81 +101,50 @@ class OSGReporter(Reporter):
             sys.exit(1)
 
     def run_report(self):
-        """Takes data from query response and parses it to send to other functions for processing"""
-        freport = self.generate_report_file()
-        freport.send(None)
-
+        """Takes data from query response and parses it to send to other
+        functions for processing"""
         results = self.runquery()
 
-        # for pname_bucket in results.group_ProjectName.buckets:
-        #     pname = pname_bucket.key
-        #     # print pname
-        #     for pi_bucket in pname_bucket.group_PIName.buckets:
-        #         pi= pi_bucket.key
-        #         # print pi
-        #         for org_bucket in pi_bucket.group_Organization.buckets:
-        #             org = org_bucket.key
-        #             # print org
-        #             for fos_bucket in org_bucket.group_FOS.buckets:
-        #                 fos = fos_bucket.key
-        #                 print pname, pi, org, fos, fos_bucket.CoreHours_sum.value
-
-        for pname_bucket in self.sorted_buckets(results.group_ProjectName, key=key_to_lower):
+        for pname_bucket in self.sorted_buckets(results.group_ProjectName,
+                                                key=key_to_lower):
             pname = pname_bucket.key
-            # print pname
             for pi_bucket in pname_bucket.group_PIName.buckets:
                 pi = pi_bucket.key
-                # print pi
                 for org_bucket in pi_bucket.group_Organization.buckets:
                     org = org_bucket.key
-                    # print org
                     for fos_bucket in org_bucket.group_FOS.buckets:
                         fos = fos_bucket.key
-                        freport.send((pname, pi, org, fos, fos_bucket.CoreHours_sum.value))
+                        yield (pname, pi, org, fos, fos_bucket.CoreHours_sum.value)
 
+    def generate_report_file(self, report):
+        pass
 
-            # p = self.pnc.getProject(projectname.key, self.report_type)
-            # if not p:
-            #     continue
-            # else:
-            #     p.setUsage(None, p.projecname.CoreHours_sum.value, 0)
-            #         # CPUHours, WallHours, njobs
-            # if self.verbose:
-            #     print projectname.key, \
-            #         p.getProjectName(), \
-            #         p.getPI(),\
-            #         p.getInstitution(),\
-            #         p.getFOS(),\
-            #         p.projectname.CoreHours_sum.value
-
-
-
-    def generate_report_file(self, report=None):
-        self.report = {}
+    def format_report(self, report=None):
+        report = {}
         for name in self.header:
-            if name not in self.report:
-                self.report[name] = []
-        try:
-            while True:
-                pname, pi, org, fos, wallhrs = yield
-                self.report["Project Name"].append(pname)
-                self.report["PI"].append(pi)
-                self.report["Institution"].append(org)
-                self.report["Field of Science"].append(fos)
-                self.report["Wall Hours"].append(wallhrs)
-        except GeneratorExit:
-            self.report["Project Name"].append("Total")
-            self.report["PI"].append("")
-            self.report["Institution"].append("")
-            self.report["Field of Science"].append("")
-            self.report["Wall Hours"].append(sum(self.report["Wall Hours"]))
-        return
-            
-    def format_report(self):
-        return self.report
-        
+            if name not in report:
+                report[name] = []
 
+        for result_tuple in self.run_report():
+            pname, pi, org, fos, wallhrs = result_tuple
+            if self.verbose:
+                print "{0}\t{1}\t{2}\t{3}\t{4}".format(pname,
+                                                       pi, org, fos,
+                                                       wallhrs)
+            report["Project Name"].append(pname)
+            report["PI"].append(pi)
+            report["Institution"].append(org)
+            report["Field of Science"].append(fos)
+            report["Wall Hours"].append(wallhrs)
 
+        if self.isSum:
+            report["Project Name"].append("Total")
+            report["PI"].append("")
+            report["Institution"].append("")
+            report["Field of Science"].append("")
+            report["Wall Hours"].append(sum(report["Wall Hours"]))
+
+        return report
 
 
 if __name__=="__main__":
@@ -206,9 +164,6 @@ if __name__=="__main__":
                         args.verbose)
         r.run_report()
         r.send_report("Project")
-        # r.format_report()
-        # r.sendReport("project")
-
     except Exception as e:
         with open(logfile, 'a') as f:
             f.write(traceback.format_exc())
