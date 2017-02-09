1216d4da3a49380b6102ef153b98e4b8af3ad183
sbhat <sbhat@fnal.gov>
2017-02-09 16:50:26 -0600

    Wrote wrapper scripts for OSG Per Site and Flocking reports

---
+++ OSG_Reports/osgpersite_run.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+LOGFILE=osgpersite_run.log
+endtime=`date +"%F %T"`
+
+function set_dates {
+        case $1 in
+                "daily") starttime=`date --date='1 day ago' +"%F %T"`;;
+                "weekly") starttime=`date --date='1 week ago' +"%F %T"`;;
+                "bimonthly") starttime=`date --date='2 month ago' +"%F %T"`;;
+                "monthly") starttime=`date --date='1 month ago' +"%F %T"`;;
+                "yearly") starttime=`date --date='1 year ago' +"%F %T"`;;
+                *) echo "Error: unknown period $period. Use weekly, monthly or yearly"
+                         exit 1;;
+        esac
+        echo $starttime
+}
+
+set_dates $1
+
+echo "START" `date` >> $LOGFILE
+
+python OSGPerSiteReporter.py -s "$starttime" -c osg.config -T template_siteusage.html -d
+
+
+if [ $? -ne 0 ]
+then
+	echo "Error sending report. Please investigate" >> $LOGFILE
+else
+	echo "Sent report" >> $LOGFILE
+fi
+ 
+echo "END" `date` >> $LOGFILE
