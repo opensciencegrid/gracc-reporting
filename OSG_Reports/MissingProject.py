6bd772196677822acb5fb08bcbf7f2f9e240d2ef
sbhat <sbhat@fnal.gov>
2017-02-27 14:06:09 -0600

    Implementing Missing Projects report.  First commit of Report code and relevant modules

---
+++ OSG_Reports/MissingProject.py
@@ -0,0 +1,244 @@
+#!/usr/bin/python
+
+import os
+import inspect
+import re
+import json
+import traceback
+import sys
+import copy
+
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+
+parentdir = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(
+            inspect.getfile(
+                inspect.currentframe()
+            )
+        )
+    )
+)
+os.sys.path.insert(0, parentdir)
+
+import Configuration
+from Reporter import Reporter, runerror
+from ProjectNameCollector import ProjectNameCollector
+
+
+MAXINT = 2**31 - 1
+
+class MissingProjectReport(Reporter):
+    def __init__(self, report_type, config, start, end=None,
+                 verbose=False, no_email=False, is_test=False):
+        Reporter.__init__(self, report_type, config, start, end, verbose, raw=False,
+                          no_email=no_email, is_test=is_test)
+
+        # self.report_type = self.validate_report_type(report_type)
+        self.header = ["Project Name", "PI", "Institution", "Field of Science"]
+        # self.logfile = logfile
+        # self.logger = self.__setupgenLogger("MissingProject")
+        # # try:
+        # #     self.client = self.establish_client()
+        # # except Exception as e:
+        #     self.logger.exception(e)
+        self.report_type = report_type
+        self.logger.info("Report Type: {0}".format(self.report_type))
+
+    def query(self):
+        """
+        Method to query Elasticsearch cluster for OSGReporter information
+
+        :return:
+        """
+
+        # Gather parameters, format them for the query
+        starttimeq = self.dateparse_to_iso(self.start_time)
+        endtimeq = self.dateparse_to_iso(self.end_time)
+
+        probes = [_.strip("'") for _ in re.split(",", self.config.get(
+            "query", "{0}_probe_list".format(self.report_type)))]
+
+        if self.verbose:
+            print probes
+        s = Search(using=self.client, index=self.indexpattern) \
+                .filter("range", EndTime={"gte": starttimeq, "lt": endtimeq}) \
+                .filter("range", WallDuration={"gt": 0}) \
+                .filter("terms", ProbeName=probes) \
+                .filter("term", ResourceType="Payload") \
+                .filter("exists", field="RawProjectName")[0:0]
+
+        self.unique_terms = ['OIM_PIName', 'RawProjectName', 'ProbeName',
+                 'Host_description', 'VOName']
+
+        curBucket = s.aggs.bucket("OIM_PIName", "missing", field="OIM_PIName")
+
+        for term in self.unique_terms[1:]:
+            curBucket = curBucket.bucket(term, "terms", field=term, size=MAXINT)
+
+        return s
+
+    def runquery(self):
+        """Execute the query and check the status code before returning the response
+
+        :return:
+        """
+        resultset = self.query()
+        t = resultset.to_dict()
+        if self.verbose:
+            print json.dumps(t, sort_keys=True, indent=4)
+            self.logger.debug(json.dumps(t, sort_keys=True))
+        else:
+            self.logger.debug(json.dumps(t, sort_keys=True))
+
+        try:
+            response = resultset.execute()
+            if not response.success():
+                raise
+            results = response.aggregations
+            self.logger.debug("Elasticsearch query executed successfully")
+            # print response
+            # print results
+            return results
+        except Exception as e:
+            print e, "Error accessing Elasticsearch"
+            sys.exit(1)
+
+    def run_report(self):
+        """
+
+        :return:
+        """
+        results = self.runquery()
+            # Some logic for if XD project, use similar logic as ProjectNameCollector does to get XD name
+            # Else, in other cases, email us.
+
+            # Also need to get more buckets (see GRACC-38 for more details)
+
+        unique_terms = self.unique_terms
+
+        def recurseBucket(curData, curBucket, index, data):
+            """
+            Recursively process the buckets down the nested aggregations
+
+            :param curData: Current parsed data that describes curBucket and will be copied and appended to
+            :param bucket curBucket: A elasticsearch bucket object
+            :param int index: Index of the unique_terms that we are processing
+            :param data: list of dicts that holds results of processing
+
+            :return: None.  But this will operate on a list *data* that's passed in and modify it
+            """
+            curTerm = unique_terms[index]
+
+            # Check if we are at the end of the list
+            if not curBucket[curTerm]['buckets']:
+                # Make a copy of the data
+                nowData = copy.deepcopy(curData)
+                data.append(nowData)
+            else:
+                # Get the current key, and add it to the data
+                for bucket in curBucket[curTerm]['buckets']:
+                    nowData = copy.deepcopy(curData)    # Hold a copy of curData so we can pass that in to any future recursion
+                    nowData[curTerm] = bucket['key']
+                    if index == (len(unique_terms) - 1):
+                        # reached the end of the unique terms
+                        # for metric in metrics:
+                        #     nowData[metric[0]] = bucket[metric[0]].value
+                            # Add the doc count
+                        nowData["Count"] = bucket['doc_count']
+                        data.append(nowData)
+                    else:
+                        recurseBucket(nowData, bucket, index + 1, data)
+
+        # print __recurse_buckets(results.OIM_PIName, {}, 1)
+        data = []
+        recurseBucket({}, results.OIM_PIName, 1, data)
+        print data
+
+        for item in data:
+            self.check_project(item)
+            # p_object = self.check_project(item['RawProjectName'])
+            # if p_object:
+            #     yield p_object
+            # else:       # What to do for N/A/ stuff?
+            #     pass
+
+
+    @staticmethod
+    def no_name(name):
+        return name == 'N/A' or name == "UNKNOWN"
+
+    def check_project(self, project):
+        """
+
+        :return:
+        """
+        PNC = ProjectNameCollector(self.config)
+        if self.no_name(project['RawProjectName']):
+            print "No Name!"
+            # Want to email OSG Support about this record
+            return None
+        else:
+            # Do checks like in ProjectNameCollector
+            # This was just a check - delete when committing
+            # PNC2 =  ProjectNameCollector(self.config)
+            # p_object = PNC2.get_project('TG-ENG150008', source=self.report_type)
+            # print p_object.get_project_name(),p_object.get_pi(),p_object.get_institution(),p_object.get_fos()
+            return PNC.get_project(project['RawProjectName'],
+                                   source=self.report_type)
+
+
+    # def format_report(self, report=None):
+        # report = {}
+        # for name in self.header:
+        #     if name not in report:
+        #         report[name] = []
+        #
+        # for p_object in self.run_report():
+        #     pname, pi, org, fos = (p_object.get_project_name(),
+        #                                     p_object.get_pi(),
+        #                                     p_object.get_institution(),
+        #                                     p_object.get_fos())
+        #     if self.verbose:
+        #         print "{0}\t{1}\t{2}\t{3}".format(pname,
+        #                                                pi, org, fos)
+        #
+        #     report["Project Name"].append(pname)
+        #     report["PI"].append(pi)
+        #     report["Institution"].append(org)
+        #     report["Field of Science"].append(fos)
+        #
+        # print report
+        # exit(0)
+        # return report
+
+
+
+if __name__ == '__main__':
+    args = Reporter.parse_opts()
+
+    config = Configuration.Configuration()
+    config.configure(args.config)
+
+    # try:
+    r = MissingProjectReport(args.report_type,
+                             config,
+                             args.start,
+                             args.end,
+                             verbose=args.verbose,
+                             is_test=args.is_test,
+                             no_email=args.no_email)
+    r.run_report()
+    # r.format_report()
+        # r.send_report("Project")
+        # r.logger.info("OSG Project Report executed successfully")
+    # except Exception as e:
+    #     with open(logfile, 'a') as f:
+    #         f.write(traceback.format_exc())
+    #     print >> sys.stderr, traceback.format_exc()
+    #     runerror(config, e, traceback.format_exc())
+    #     sys.exit(1)
+    sys.exit(0)
+
+
---
+++ OSG_Reports/ProjectNameCollector.py
@@ -0,0 +1,180 @@
+"""This module collects project information for various sources OIM, XD DB"""
+
+import sys
+import optparse
+import os
+import inspect
+
+parentdir = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(
+            inspect.getfile(
+                inspect.currentframe()
+            )
+        )
+    )
+)
+os.sys.path.insert(0, parentdir)
+
+import Configuration
+from XDProject import XDProject
+from OSGProject import OSGProject
+from ProjectName import ProjectName
+
+__author__ = "Tanya Levshina"
+__email__ = "tlevshin@fnal.gov"
+
+
+class ProjectNameCollector:
+    """Collects and create container of Projects from various sources"""
+
+    def __init__(self, config, verbose=False):
+        """ Collects ProjectName information from all available sources (OIM, XD DB).
+        Args:
+            config(Configuration)
+            verbose(boolean) - debug messages flag
+        """
+        self.projects = {}
+        self.config = config
+        self.verbose = verbose
+        # Known projects are stored in a file, name of the file is under project_name csv in configuration
+        # self.cache = self.config.config.get("project_name", "csv")
+        # self.parse()
+
+    # def parse(self):
+    #     """Parses the cache file with project names, skip failures"""
+    #
+    #     try:
+    #         lines = open(self.cache).readlines()
+    #     except IOError, e:
+    #         print >> sys.stderr, e
+    #         sys.exit(1)
+    #     for l in lines:
+    #         # Abbott, Bill, Rutgers; the State University of New Jersey, TG-TRA120035, Training, OSG, 0, 0, 0
+    #         try:
+    #             tmp = l[:-1].split(",")
+    #             p = ProjectName(tmp[3])
+    #             p.set_first_name(tmp[1])
+    #             p.set_last_name(tmp[0])
+    #             p.set_pi("%s %s" % (tmp[0], tmp[1]))
+    #             p.set_institution(tmp[2])
+    #             p.set_fos(tmp[4])
+    #             p.set_id(tmp[6])
+    #             self.projects[tmp[3]] = p
+    #         except:
+    #             print >> sys.stderr, "Can not parse ", l
+    #             continue
+
+    def get_project(self, name, source):
+        """Finds project in XD Database or OIM if it is not in cache
+        Args:
+            name(str) - name of the project
+            source(str) - XD,  OSG or OSG-Connect
+        """
+        if source == "XD":
+            # print name
+            if not name.startswith("TG-"):
+                return None
+        else:   # Redundant check
+            if name.startswith("TG-"):
+                return None
+        # if self.projects.has_key(name):
+        #     if self.verbose:
+        #         print >> sys.stdout, "Project is in cache ", name
+        #     return self.projects[name]
+
+        # # if project is in OIM just get it from there
+        # oim = OSGProject(name, self.config)
+        # if not oim.execute_query(url_type='project'):
+        #     self.projects[name] = oim
+        #     # Add new project to cache
+        #     self.add_to_cache(self.projects[name])
+        #     return self.projects[name]
+
+        # Now, project is not in OIM. If this is OSG project we could not do anything but send notification,
+        # if this is XD project we could try XD database
+        if source == "XD":
+            xd = XDProject(name, self.config, self.verbose)
+            if not xd.execute_query(url_type='project'):
+                    # Email Mats about this project
+                    print "This project is not in XD database ", name
+            else:
+                # Put the information in request file, will be sent later
+                # self.create_request_to_register_oim(name, source, xd)
+
+                # We will still report this project
+                self.projects[name] = xd
+                return self.projects[name]
+        else:
+            print >>sys.stderr, "This project %s is not registered " % (name,)
+            # Put the information in request file, will be sent later
+            self.create_request_to_register_oim(name, source)
+        return None
+
+
+    # def add_to_cache(self, p):
+    #     """Adds project and relative info to cache csv file
+    #     Args:
+    #         p(Project)
+    #     """
+    #     try:
+    #         if self.verbose:
+    #                 print >> sys.stdout, "Trying to add project to cache ", p.name
+    #         fd = open(self.cache, "a")
+    #         fd.write("%s,%s,%s,%s,%s,OSG,%s,0,0\n" % (p.get_last_name().replace(",", ";"), p.get_first_name(),
+    #                                                       p.get_institution().replace(", ", ";"),
+    #                                                       p.get_project_name().replace(",", ";"),
+    #                                                       p.get_fos().replace(",", ";"), p.get_id()))
+    #         fd.close()
+    #     except:
+    #         print >>sys.stderr, "Failed add to cache", self.cache
+
+    def create_request_to_register_oim(self, name, source, p=None):
+        """Creates file with information related to project that will be sent later to OSG secretary
+        Args:
+            name(str) - project name
+            source(str) - XD, OSG, or  OSG-Connect"
+            p(Project) - project
+        """
+ 
+        if self.projects.has_key(name):
+            return
+        if source == "XD" and name.startswith("TG-"):
+            f = open("OIM_Project_Name_Request_for_XD", "a")
+            f.write("****************START****************\n")
+            f.write("ProjectName: %s\nPI: %s\nEmail: %s\nInstitution: %s\nDepartment: %s\nField of Science: %s\nDescription: %s\n" %
+                    (name, p.get_pi(), p.get_email(), p.get_institution(), p.get_department(), p.get_fos(),
+                     p.get_abstract()))
+            f.write("****************END****************\n")
+            f.close()
+        else:
+            f = open("OIM_Project_Name_Request_for_%s" % (source,), "a")
+            f.write("Project names that are reported from %s but not registered  in OIM\n" % (source, ))
+            f.write("ProjectName: %s\n" % (name,))
+            f.close()
+
+
+def parse_opts():
+    """Parses command line options"""
+
+    usage = "Usage: %prog [options] ProjectName"
+    parser = optparse.OptionParser(usage)
+    parser.add_option("-c", "--config", dest="config", help="report configuration file (required)")
+    parser.add_option("-r", "--report", dest="report_type", help="report type: XD,OSG or OSG-Connect",
+                      choices=["XD", "OSG", "OSG-Connect", "Duke-Connect"], default="OSG")
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False,
+                      help="print debug messages to stdout")
+    opts, args = parser.parse_args()
+    if len(args) != 1:
+        parser.print_usage()
+        sys.exit(1)
+    Configuration.checkRequiredArguments(opts, parser)
+    return opts, args
+
+if __name__ == "__main__":
+    opts, args = parse_opts()
+    config = Configuration.Configuration()
+    config.configure(opts.config)
+    pnc = ProjectNameCollector(config, opts.verbose)
+    p = pnc.get_project(args[0], opts.report_type)
+    print p.__dict__
