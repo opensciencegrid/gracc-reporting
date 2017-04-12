4345150deb807dad9035decc7c704918426b2792
sbhat <sbhat@fnal.gov>
2017-04-12 15:57:59 -0500

    Updated setup.py to include config and HTML template files

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup
 
 setup(name='gracc-reporting',
-        version='0.1dev',
-        description='GRACC Email Reports',
-        author_email='sbhat@fnal.gov',
-        author='Shreyas Bhat',
-        url='https://github.com/opensciencegrid/gracc-reporting',
-        package_dir={'': 'src'},
-        packages=['reports', 'reports.fife_reports', 'reports.osg_reports'],
-        entry_points= {
-            'console_scripts': [
-                'efficiencyreport = reports.fife_reports.EfficiencyReporterPerVO:main'
+      version='0.1dev',
+      description='GRACC Email Reports',
+      author_email='sbhat@fnal.gov',
+      author='Shreyas Bhat',
+      url='https://github.com/opensciencegrid/gracc-reporting',
+      package_dir={'': 'src'},
+      packages=['reports', 'reports.fife_reports', 'reports.osg_reports'],
+      package_data={
+          '':['*.config', 'template*.html']
+      },
+      entry_points= {
+          'console_scripts': [
+              'efficiencyreport = reports.fife_reports.EfficiencyReporterPerVO:main'
             ]
-        }
+      }
       )
