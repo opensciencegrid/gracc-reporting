5178f17826cc7599e29f55d7a8cc0f71aa33b845
sbhat <sbhat@fnal.gov>
2017-04-25 15:12:41 -0500

    Updated license and bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gracc-reporting',
-      version='0.1',
+      version='0.2',
       description='GRACC Email Reports',
       author_email='sbhat@fnal.gov',
       author='Shreyas Bhat',
