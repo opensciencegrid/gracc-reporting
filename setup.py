dbfb79c27fa4b388b70b317cf86e007a3508c5b5
sbhat <sbhat@fnal.gov>
2017-05-08 13:31:38 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gracc-reporting',
-      version='0.3',
+      version='0.4',
       description='GRACC Email Reports',
       author_email='sbhat@fnal.gov',
       author='Shreyas Bhat',
