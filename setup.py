55de125d9e5b52c4af1e2d4d31d8d30f305088b8
sbhat <sbhat@fnal.gov>
2017-05-10 10:16:41 -0500

    Enforcing python version

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup
+import sys
+
+# Enforce python version
+version_tuple = (2, 7)
+if sys.version_info < version_tuple:
+    print "Sorry, installing gracc-reporting requires Python {0}.{1} " \
+          "or above".format(*version_tuple)
+    exit(1)
 
 setup(name='gracc-reporting',
       version='0.4',
