d308e71e059853d750cc8b22d7b1562f65bc9142
sbhat <sbhat@fnal.gov>
2017-05-15 11:02:43 -0500

    Bumped version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info < version_tuple:
     exit(1)
 
 setup(name='gracc-reporting',
-      version='0.5',
+      version='0.6',
       description='GRACC Email Reports',
       author_email='sbhat@fnal.gov',
       author='Shreyas Bhat',
