0ed1c2edb1c70d46e96543c8f42f8232abacc561
sbhat <sbhat@fnal.gov>
2017-05-17 15:43:54 -0500

    Took out some debugging print statements

--- src/graccreports/ReportUtils.py
+++ src/graccreports/ReportUtils.py
@@ -174,10 +174,6 @@ class Reporter(TimeUtils):
         htmldata = emailReport.printAsTextTable("html", content,
                                                 template=self.template)
 
-        for key, value in text.iteritems():
-            print key, type(value)
-
-
         if self.header:
             htmlheader = unicode("\n".join(['<th>{0}</th>'.format(headerelt)
                                     for headerelt in self.header]), 'utf-8')
