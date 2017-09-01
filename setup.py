c5e63665ba5253d05d23966ff9856d9b31ff3779
sbhat <sbhat@fnal.gov>
2017-09-01 17:06:33 -0500

    Updated version to 1.0.dev0 to fulfill PEP 440

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info < version_tuple:
     exit(1)
 
 setup(name='gracc-reporting',
-      version='1.0dev',
+      version='1.0.dev0',
       description='GRACC Email Reports',
       author_email='sbhat@fnal.gov',
       author='Shreyas Bhat',
