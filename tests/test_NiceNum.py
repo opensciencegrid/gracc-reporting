d11a5a424d5e40a15d4ec92c6562b6a508ed7010
sbhat <sbhat@fnal.gov>
2018-04-04 10:41:37 -0500

    Moved module import.  Also Added test configs to repo

--- tests/test_NiceNum.py
+++ tests/test_NiceNum.py
@@ -1,9 +1,13 @@
+"""Unit tests for NiceNum"""
+
 import unittest
 import doctest
+
+import gracc_reporting.NiceNum as NiceNum
+
 
 class TestNiceNum(unittest.TestCase):
     """Test niceNum from NiceNum"""
     def test_nicenum_from_doctest(self):
         """Run doctests in niceNum"""
-        import gracc_reporting.NiceNum as NiceNum
         doctest.testmod(NiceNum, verbose=False)
