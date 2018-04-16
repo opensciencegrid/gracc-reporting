3f5ff2ef7e9d041956be035cde603b806254bb65
sbhat <sbhat@fnal.gov>
2018-04-16 16:48:04 -0500

    Cleared out __init__.py

--- gracc_reporting/__init__.py
+++
@@ -1,5 +0,0 @@
-# import Configuration
-# from ReportUtils import Reporter, runerror, get_configfile, get_template, coroutine
-from ReportUtils import Reporter, runerror, coroutine
-import TextUtils
-import NiceNum
