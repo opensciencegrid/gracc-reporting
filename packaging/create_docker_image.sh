460b1f9252715842b86ca69da1cabfea139e5450
sbhat <sbhat@fnal.gov>
2018-06-28 13:59:19 -0500

    Docker image moved to alpine.  This eliminates need for spec and lots of patching, and reduces the size of the image.

--- packaging/create_docker_image.sh
+++ packaging/create_docker_image.sh
@@ -3,11 +3,14 @@
 
 # Build the RPM
 START=${PWD}
-SRCSPEC=${START}/gracc-reporting.spec
-VERSION=`grep -m 1 version ${SRCSPEC} | awk '{print $3}'`
+SETUP_DOT_PY=${PWD}/../setup.py
+
+VERSION=`grep -E "version=\'.+\'\,$" ${SETUP_DOT_PY}`  # VERSION will be something like "version='2.0'," without surrounding "
+VERSION=${VERSION%\'*}	# Remove trailing quotation, comma
+VERSION=${VERSION#*\'}	# Remove everything up until the first quotation
 
 if [[ "x$VERSION" == "x" ]] ; then 
-	echo "Version is not properly set in spec file.  Exiting"
+	echo "Version is not properly set in the setup.py file.  Exiting"
 	exit 1
 else 
 	echo "VERSION $VERSION"
