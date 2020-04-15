21517cabb0affbda149d2226d3840a9f55605dda
Derek Weitzel <djw8605@gmail.com>
2020-04-15 18:56:47 -0500

    Adding alignment

--- gracc_reporting/TextUtils.py
+++ gracc_reporting/TextUtils.py
@@ -53,11 +53,15 @@ class TextUtils:
         # Order the columns according to the header
         df = df[self.table_header]
 
+        alignment_list = ["left"] * len(self.table_header) - 1
+        alignment_list.append("right")
         # the order is defined by header list
         if format_type == "text":
-            return tabulate.tabulate(df, tablefmt="grid", headers=self.table_header, showindex=False, floatfmt=',.1f')
+            return tabulate.tabulate(df, tablefmt="grid", headers=self.table_header,
+                                     showindex=False, floatfmt=',.0f', colalign=alignment_list)
         elif format_type == "html":
-            return tabulate.tabulate(df, tablefmt="html", headers=self.table_header, showindex=False, floatfmt=',.1f')
+            return tabulate.tabulate(df, tablefmt="html", headers=self.table_header,
+                                     showindex=False, floatfmt=',.0f', colalign=alignment_list)
         elif format_type == "csv":
             return df.to_csv(index=False)
 
