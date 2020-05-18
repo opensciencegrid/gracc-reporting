dd30d842d240de65b65dd67ba79aa4de6f49ba9e
Derek Weitzel <djw8605@gmail.com>
2020-05-18 15:47:03 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup
 
 setup(name='gracc-reporting',
-      version='3.0.3',
+      version='3.1.0',
       description='GRACC Email Reports',
       author_email='sbhat@fnal.gov',
       author='Shreyas Bhat',
